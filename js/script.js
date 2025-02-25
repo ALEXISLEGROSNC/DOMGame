17a2ad37de57e4beb10c6479e0561ac64ff2f4b5
AlexisL <alexislegrosnc@gmail.com>
2025-02-25 11:28:46 +0100

    feat (script.js) : moveleft | rubiks

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ function generateGrid(grid) {
     cell.setAttribute('data-y', currentY);
     cell.classList.add('c' + (currentX + 1));
 
+
     grid.appendChild(cell);
     currentX++;
     if (currentX == 5) {
@@ -28,22 +29,44 @@ function generateGrid(grid) {
       currentY++;
     }
   }
+
+  attachOnclickEvents(grid);
 }
 
+function attachOnclickEvents(grid) {
+  var elements = grid.getElementsByTagName('div');
+  Array.from(elements).forEach(element => {
+    element.onclick = function() {
+      var cell = CellFromDom(element);
+      moveLeft(element);
+    };
+  });
+}
 
-function moveUp(cell) {
+function moveUp(domCell) {
 //todo
 }
 
-function moveDown(cell) {
+function moveDown(domCell) {
 //todo
 }
 
-function moveLeft(cell) {
-//todo
+function moveLeft(domCell) {
+  
+  var grid = document.getElementById("grille");
+  var domCells = grid.getElementsByTagName('div');
+  
+  var rowCells = Array.from(domCells).filter(element => parseInt(element.dataset.y) === parseInt(domCell.dataset.y));
+
+  rowCells.forEach(cell => {
+    var x = parseInt(cell.dataset.x);
+    cell.dataset.x = (x + 1) % 5;
+  });
+
+  refreshPositions(grid);
 }
 
-function moveRight(cell) {
+function moveRight(domCell) {
 //todo
 }
 
@@ -52,7 +75,6 @@ function refreshPositions(grid){
   var elements = grid.getElementsByTagName('div');
   Array.from(elements).forEach(element => {
     element.style.transform = "translate(" + (element.dataset.x * 100) + "px," + (element.dataset.y * 100) + "px)";
-    console.log("translate(" + (element.dataset.x * 20) + "px," + (element.dataset.y * 20) + "px)");
   });
 }
 
@@ -60,10 +82,27 @@ function randomizeGrid(grid){
 //todo
 }
 
-function getDomCellForCoordinates(x,y){
-//todo
+function DomFromCell(x, y) {
+  var grid = document.getElementById("grille");
+  var elements = grid.getElementsByTagName('div');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    if (parseInt(element.getAttribute('data-x')) === x && parseInt(element.getAttribute('data-y')) === y) {
+      return element;
+    }
+  }
+  return null; // Si aucun élément n'est trouvé
 }
 
-function checkForWin(grid){
+function CellFromDom(domCell) {
+  return {
+    x: parseInt(domCell.getAttribute('data-x')),
+    y: parseInt(domCell.getAttribute('data-y'))
+  };
+}
+
+
+
+window.checkForWin = function(grid){
 //todo 
 }
