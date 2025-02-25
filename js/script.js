a65725d5f7ee329c162087c018dd89e89837417d
AlexisL <alexislegrosnc@gmail.com>
2025-02-25 10:49:47 +0100

    feat (script.js) : new grid | rubik's carré

--- js/script.js
+++ js/script.js
@@ -1,18 +1,64 @@
-import Grille from "./Grille.js";
-
-// 1 On définit une sorte de "programme principal"
-// le point d'entrée du code qui sera appelée dès que la
-// page ET SES RESSOURCES sont chargées
-
 window.onload = init;
 
-let grille;
 
 function init() {
   console.log("Page et ressources prêtes à l'emploi");
-  // appelée quand la page et ses ressources sont prêtes.
-  // On dit aussi que le DOM est ready (en fait un peu plus...)
+  var grid = document.getElementById("grille");
+  generateGrid(grid);
+  randomizeGrid(grid);
+  refreshPositions(grid);
+}
 
-  grille = new Grille(9, 9);
-  grille.showCookies();
+
+function generateGrid(grid) {
+  grid.innerHTML = '';
+  var currentX = 0;
+  var currentY = 0;
+  for (var i = 0; i < 5 * 5; i++) {
+    var cell = document.createElement('div');
+    cell.setAttribute('data-x', currentX);
+    cell.setAttribute('data-y', currentY);
+    cell.classList.add('c' + (currentX + 1));
+
+    grid.appendChild(cell);
+    currentX++;
+    if (currentX == 5) {
+      currentX = 0;
+      currentY++;
+    }
+  }
+}
+
+
+function moveUp(cell) {
+//todo
+}
+
+function moveDown(cell) {
+//todo
+}
+
+function moveLeft(cell) {
+//todo
+}
+
+function moveRight(cell) {
+//todo
+}
+
+
+function refreshPositions(grid){
+  var elements = grid.getElementsByTagName('div');
+  Array.from(elements).forEach(element => {
+    element.style.transform = "translate(" + (element.dataset.x * 100) + "px," + (element.dataset.y * 100) + "px)";
+    console.log("translate(" + (element.dataset.x * 20) + "px," + (element.dataset.y * 20) + "px)");
+  });
+}
+
+function randomizeGrid(grid){
+//todo
+}
+
+function getDomCellForCoordinates(x,y){
+//todo
 }
